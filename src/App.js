007f59bbfb351ccd8d4b58a31abdadfe2709d426
Bhavik Patel <patelbj2004@gmail.com>
2024-01-24 14:19:25 +0530

    added functionality that fetch use category wise

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
  
   render() {
     return (
       <div>
-        <NavBar/>
-        <News pageSize={3} country='in' category='business'/>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<NavBar/>}>
+          <Route index element={<News pageSize={3} key='general' country='in' category='general'/>} />
+          <Route exact path="business"  element={<News pageSize={3} country='in' key='business' category='business'/>} />
+          <Route exact path="entertainment"  element={<News pageSize={3} country='in' key='entertainment' category='entertainment'/>} />
+          <Route exact path="science"  element={<News pageSize={3} country='in' key='science' category='science'/>} />
+          <Route exact path="sports" element={<News pageSize={3} country='in' key='sports' category='sports'/>} />
+          <Route exact path="technology" element={<News pageSize={3} country='in'  key='technology' category='technology'/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
+        
+        
+        
+        
+        
+        
+        
+        
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import imgNotfound from './image-not-found.png'
 
 export default class NewsItem extends Component {
   render() {
@@ -6,11 +7,11 @@ export default class NewsItem extends Component {
     return (
       <div>
         <div className="card my-2">
-            <img src={imageUrl?imageUrl:"https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"} className="card-img-top" alt="..."/>
+            <img src={imageUrl?imageUrl:imgNotfound} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank"  className="btn btn-sm btn-primary">Read more</a>
+              <a href={newsUrl} target="_blank" rel="noreferrer"  className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
